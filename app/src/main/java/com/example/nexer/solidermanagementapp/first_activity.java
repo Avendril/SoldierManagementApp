8688fa8066a55e3043cbc0bb439225eef5007b4f
Bartosz Chmielewski <nexerio7991@gmail.com>
2018-03-06 14:22:52 +0000

    comments

--- app/src/main/java/com/example/nexer/solidermanagementapp/first_activity.java
+++ app/src/main/java/com/example/nexer/solidermanagementapp/first_activity.java
@@ -1,13 +1,20 @@
 package com.example.nexer.solidermanagementapp;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.programmingknowledge.mysqldemo.R;
 
+/*
+References:
+https://developer.android.com/guide/topics/ui/notifiers/toasts.html <- Message displayed
+
+ */
 public class first_activity extends AppCompatActivity {
 
     private Button button;
@@ -15,11 +22,16 @@ public class first_activity extends AppCompatActivity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_activity);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_first_activity);
 
-        UsernameET = (EditText)findViewById(R.id.loginTextBox);
-        PasswordET = (EditText)findViewById(R.id.passwordTextBox);
+            UsernameET = (EditText) findViewById(R.id.loginTextBox);
+            PasswordET = (EditText) findViewById(R.id.passwordTextBox);
+        }catch(Exception e)
+        {
+            Toast toast = Toast.makeText(getApplicationContext(),"This shit ain't working yo", Toast.LENGTH_SHORT);
+        }
     }
 
     public void OnLogin(View view){
